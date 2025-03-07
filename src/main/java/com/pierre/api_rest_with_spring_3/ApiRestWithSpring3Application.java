747156db4a1e3080e25276c89a8599f9eb56f141
pierre enoc <pierre_enoc@yahoo.fr>
2025-03-06 23:00:09 -0300

    project created

---
+++ src/main/java/com/pierre/api_rest_with_spring_3/ApiRestWithSpring3Application.java
@@ -0,0 +1,13 @@
+package com.pierre.api_rest_with_spring_3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiRestWithSpring3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiRestWithSpring3Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/pierre/api_rest_with_spring_3/ApiRestWithSpring3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pierre.api_rest_with_spring_3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiRestWithSpring3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
