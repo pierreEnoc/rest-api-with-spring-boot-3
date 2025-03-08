42c17f903934a59aa52d239568a3a1afac3d20d4
pierre enoc <pierre_enoc@yahoo.fr>
2025-03-08 15:47:43 -0300

    refactor exception

--- src/main/java/com/pierre/api_rest_with_spring_3/exception/ResourceNotFoundException.java
+++ src/main/java/com/pierre/api_rest_with_spring_3/exception/ResourceNotFoundException.java
@@ -4,9 +4,9 @@ package com.pierre.api_rest_with_spring_3.exception;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus(HttpStatus.BAD_REQUEST)
+@ResponseStatus(HttpStatus.NOT_FOUND)
 public class ResourceNotFoundException extends  RuntimeException {
     public ResourceNotFoundException(String message) {
-
+       super(message);
     }
 }
